57567b7263a27b91ede070f69e580347846ac7c3
Riya Alex <riyaalexx@gmail.com>
2026-05-21 14:24:47 +0200

    code cleanup and minor changes

--- pigLatin.js
+++ pigLatin.js
@@ -6,6 +6,9 @@ const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 let finalstring = "";
 for (let i = 0; i < wordsList.length; i++) {
   let tempWord = "";
+  const isUpper = wordsList[i][0] === wordsList[i][0].toUpperCase();
+  wordsList[i] = wordsList[i].toLowerCase();
+  console.log(wordsList[i]);
   if (!vowels.includes(wordsList[i][0]) && vowels.includes(wordsList[i][1])) {
     tempWord = wordsList[i].slice(1) + wordsList[i][0] + "ay";
   } else if (
@@ -16,6 +19,7 @@ for (let i = 0; i < wordsList.length; i++) {
   } else if (vowels.includes(wordsList[i][0])) {
     tempWord = wordsList[i] + "way";
   }
+  if (isUpper) tempWord = tempWord[0].toUpperCase() + tempWord.slice(1);
   tempWord += " ";
   finalstring += tempWord;
 }
